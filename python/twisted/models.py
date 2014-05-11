bd57e05b2ad65f7a14bd52780ecbc46b8de6daa4
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2014-05-12 00:04:56 +0200

    testing valid response

---
+++ python/twisted/models.py
@@ -0,0 +1,18 @@
+from structures import CaseInsensitiveDict
+
+
+class HeaderDict(CaseInsensitiveDict):
+    pass
+
+
+class Request(object):
+
+    def __init__(self, command, path, version):
+        self.command = command
+        self.version = version
+        self.path = path
+        self.headers = HeaderDict()
+
+    def __repr__(self):
+        args = self.command, self.path, self.version, self.headers
+        return '<Request({},{},{})>\n -- {}'.format(*args)
--- python/twisted/serve.py
+++ python/twisted/serve.py
@@ -1,54 +1,68 @@
 from twisted.internet.protocol import ServerFactory
 from twisted.protocols.basic import LineReceiver
-from twisted.python import log
+#from twisted.python import log
+from models import Request
 
 
 class HTTPProtocol(LineReceiver):
 
-    #delimiter = b'\n'  # defaults to b'\r\n'
-    is_first_line = True
+    request = None
+    _cheader = ''
 
     def lineReceived(self, line):
 
-        if line == b'':
-            self.transport.write('HTTP/1.1 200 OK\n')
-            self.transport.write('Content-Length: 42\n')
-            self.transport.write('Content-Type: text/plain\n\r\n\r')
-            self.transport.write('abcdefghijklmnoprstuvwxyz1234567890abcdef')
-            #self.transport.loseConnection()
-            print('hej')
-        return
+        if not self.request:
+            parts = line.split()
+            if len(parts) == 3:
+                self.request = Request(*parts)
+            else:
+                self.badRequest()
+                return
 
-#        if self.is_first_line:
-#            parts = line.split()
-#            if len(parts) != 3:
-#                self.transport.write('HTTP/1.1 400 Bad Request\n\r\n\r')
-#                self.transport.loseConnection()
-#                return
-#
-#            command, request, version = parts
-#            self._command = command
-#            self._path = request
-#            self._version = version
-#            self.is_first_line = False
-#            self.sendLine(line)
-#        elif line == b'':
-#            print('done')
-#            self.transport.loseConnection()
-#        else:
-#            self.sendLine('H: ' + line)
+        elif line == '':
+            if self._cheader:
+                self.headerReceived(self._cheader)
+            self.emptyLineReceived()
+        elif line[0] in ' \t':
+            self._header += '\n' + line
+        elif ':' in line:
+            if self._cheader:
+                self.headerReceived(self._cheader)
+            self._cheader = line
 
-        #self.transport.loseConnection()
+    def headerReceived(self, rawheader):
+        try:
+            key, value = rawheader.split(":", 1)
+            self.request.headers[key] = value
+        except:
+            self.badRequest()
 
+    def emptyLineReceived(self):
+        print('emptyLineReceived')
+        print(self.request)
+        self.transport.loseConnection()
 
+    def badRequest(self):
+        self.transport.write(b'HTTP/1.1 400 Bad Request\n\r\n\r')
+        self.transport.loseConnection()
 
 
 class HTTPFactory(ServerFactory):
     protocol = HTTPProtocol
 
+
 if __name__ == "__main__":
     from twisted.internet import reactor
-    import sys
-    #log.startLogging(sys.stdout)
+    # import sys
+    # log.startLogging(sys.stdout)
     reactor.listenTCP(8080, HTTPFactory())
     reactor.run()
+
+#        if line == b'':
+#            self.transport.write('HTTP/1.1 200 OK\n')
+#            self.transport.write('Content-Length: 42\n')
+#            self.transport.write('Content-Type: text/plain\n\r\n\r')
+#            self.transport.write('abcdefghijklmnoprstuvwxyz1234567890abcdef')
+#            #self.transport.loseConnection()
+#            print('hej')
+#        return
